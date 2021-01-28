99f7af5dc0ae774bd1313fc8274308f7ca5243e5
Aman Nanda <34944818+amannanda8@users.noreply.github.com>
2021-01-28 11:07:18 +0530

    Final commit

--- intershalaBot.py
+++ intershalaBot.py
@@ -1,84 +1,95 @@
 import requests
-import json
 from time import sleep
 from selenium import webdriver
-# # Getting temp email id
-baseURL = 'https://www.1secmail.com/api/v1/?action='
-authBaseUrl = 'https://internshala.com/registration'
-targetWebsite = 'https://internshala.com/3-day-winternship-camp?utm_source=refer_whatsapp&utm_medium=10508427'
-user_first_name = 'Yaman'
-user_last_name = 'Sharma'
+from selenium.webdriver.firefox.options import Options
+
+# How many users do you want?
+count = 100
 
 
-count = 1
-# Get request for 1 email id
-def new_func(baseURL, count):
-    getEmail = requests.get(f'{baseURL}genRandomMailbox&count={count}')
-    emailInJson = getEmail.json()
-    email = emailInJson[0]
-    return email
+def main():
+    # # Getting temp email id
+    baseURL = 'https://www.1secmail.com/api/v1/?action='
+    authBaseUrl = 'https://internshala.com/registration'
+    targetWebsite = 'https://internshala.com/3-day-winternship-camp?utm_source=refer_whatsapp&utm_medium=10508427'
+    user_first_name = 'Yaman'
+    user_last_name = 'Sharma'
 
-email = ''
-while True:
-    email = new_func(baseURL, count)
-    if(email.find('1secmail') == -1):
-        break
-print(email)
+    count = 1
+    # Get request for 1 email id
 
-# # Opening browser
+    def new_func(baseURL, count):
+        getEmail = requests.get(f'{baseURL}genRandomMailbox&count={count}')
+        emailInJson = getEmail.json()
+        email = emailInJson[0]
+        return email
 
-browser = webdriver.Firefox()
-browser.implicitly_wait(5)
+    email = ''
+    while True:
+        email = new_func(baseURL, count)
+        if(email.find('1secmail') == -1):
+            break
+    print(email)
 
-browser.get(targetWebsite)
+    # # Opening browser
+    options = Options()
+    options.headless = True
 
-email_input = browser.find_element_by_id('email')
-password_input = browser.find_element_by_id('password')
-first_name_input =  browser.find_element_by_id('first_name')
-last_name_input =  browser.find_element_by_id('last_name')
+    browser = webdriver.Firefox(options=options)
+    browser.implicitly_wait(5)
+
+    browser.get(targetWebsite)
+
+    email_input = browser.find_element_by_id('email')
+    password_input = browser.find_element_by_id('password')
+    first_name_input = browser.find_element_by_id('first_name')
+    last_name_input = browser.find_element_by_id('last_name')
+
+    email_input.send_keys(email)
+    password_input.send_keys('WeAreAnonymous777')
+    first_name_input.send_keys(user_first_name)
+    last_name_input.send_keys(user_last_name)
+
+    sleep(1)
+
+    login_link = browser.find_element_by_xpath(
+        '//*[@id="registration-form"]/button')
+    login_link.click()
+
+    sleep(10)
+
+    # Spliting the email into login and domain
+    emailList = email.split('@')
+    login = emailList[0]
+    domain = emailList[1]
+
+    # input('Press any key to continue\n')
+
+    # Getting message id
+    getMessage = requests.get(
+        f'{baseURL}getMessages&login={login}&domain={domain}')
+    messageInJson = getMessage.json()
+    messageID = messageInJson[0]["id"]
+
+    # Reading message with
+    readMessage = requests.get(
+        f'{baseURL}readMessage&login={login}&domain={domain}&id={messageID}')
+
+    readMessageInJson = readMessage.json()
+
+    # Extracting url
+    bodyHTML = readMessageInJson['body']
+    twoPartsBody = bodyHTML.split(authBaseUrl)
+    twoPartsBody = twoPartsBody[1]
+    twoPartsBody = twoPartsBody.split('"')
+    authLink = twoPartsBody[0]
+    authLink = authBaseUrl + authLink
+
+    browser.get(authLink)
+    sleep(2)
+    browser.quit()
 
 
-
-email_input.send_keys(email)
-password_input.send_keys('WeAreAnonymous777')
-first_name_input.send_keys(user_first_name)
-last_name_input.send_keys(user_last_name)
-
-sleep(1)
-
-login_link = browser.find_element_by_xpath('//*[@id="registration-form"]/button')
-login_link.click()
-
-sleep(10)
-
-# Spliting the email into login and domain
-emailList = email.split('@')
-login = emailList[0]
-domain = emailList[1]
-
-# input('Press any key to continue\n')
-
-# Getting message id
-getMessage = requests.get(f'{baseURL}getMessages&login={login}&domain={domain}')
-messageInJson = getMessage.json()
-messageID = messageInJson[0]["id"]
-
-# Reading message with 
-readMessage = requests.get(f'{baseURL}readMessage&login={login}&domain={domain}&id={messageID}')
-
-readMessageInJson = readMessage.json()
-print(readMessageInJson["date"])
-
-# Extracting url
-bodyHTML = readMessageInJson['body']
-twoPartsBody = bodyHTML.split(authBaseUrl)
-twoPartsBody = twoPartsBody[1]
-twoPartsBody = twoPartsBody.split('"')
-authLink = twoPartsBody[0]
-authLink = authBaseUrl + authLink
-
-browser.get(authLink)
-sleep(2)
-browser.quit()
-
-
+if __name__ == "__main__":
+    for i in range(count):
+        main()
